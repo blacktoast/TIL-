30c09fd05ebba03ffa02b90487f2b18a9f648715
blakctoast <dbstjd100@naver.com>
2020-04-27 18:15:15 +0900

    test

---
+++ algo/3085-사탕게임.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <cmath>
+
+using namespace std;
+
+int N;
+string can[50];
+int main()
+{
+    cin >>N;
+    for(int i=0;i<N;i++)
+    {
+        cin >> can[i];
+    }
+    cout <<can[0];
+
+}
